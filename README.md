a7ece4b80390d7ff98405e5a5d0ae9e45e14bedc
masamasaowl <aryancheers@gmail.com>
2024-12-06 20:37:00 +0530

    added the view route

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Quora-backend-practice
 This is another practice session incorporating the concept of RESTful APIs. 
 The backend of Quora is dynamic making the project challenging and fun,
-not a lot of focus is given styling-wise
+not a lot of focus is given to styling
--- index.js
+++ index.js
@@ -21,45 +21,51 @@ app.listen(port, () => {
 
 //  ============================================= 
 
-// homepage
-app.get("/", (req,res) => {
-    res.render("home")
-})
-
 
 // pseudo databse
 let posts = [
     {
+        id : "1a",
         username : "apnaCollege",
         content : "I love coding!"
     },
     {
+        id : "2b",
         username : "shraddhaMa'am",
         content : "Hard work is important,"
     },
     {
+        id : "3c",
         username : "amanSir",
         content : "Do what you love!"
     },
     {
+        id : "4d",
         username : "masamasaowl",
         content : "Working on backend Quora!"
     }
-]
+];
 
+
+// ======================== PATHS =====================
+
+// homepage
+app.get("/", (req,res) => {
+    res.render("home")
+});
 
 // ====================== Index Route =================
 // to get data of all post 
 app.get("/posts", (req, res) => {
     res.render("index", {posts})
-})
+});
 
 
 // ==================== Create Route =================
 // a form to accept the new username and content 
 app.get("/posts/new", (req,res) => {
-    res.render("newUser", );
-})
+    res.render("newUser" );
+});
 
 // we set action of form as /posts so we expect a POST request (method) on path /posts
 app.post("/posts", (req,res) => {
@@ -69,13 +75,32 @@ app.post("/posts", (req,res) => {
 
     // push data into the array by push method
     // posts.push({username,content});
-    
+
     //or to push at start select index [0]
     posts[0] = {username,content}; 
 
     // now automatically redirect to /posts 
     res.redirect('/posts');
-})
+    // see instead of 
+    // res.send("your post was created") we simply redirect it after creating the new object in the array
+});
  
 
+// ====================== View route ================
+
+// we retrive our id via GET request
+app.get("/posts/:id", (req,res) => {
+
+    // when we type /posts/1a it is stored
+    let {id} = req.params;
+
+    // we use the find method of array (p is the parameter representing the array)
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+
+    // when the requested id is entered, we render back the alotted post 
+    // the single post is stored in show.ejs
+    res.render("show", {post})
+});
  
+
